5a20506be945dbe7815874a3e8b15c430ec1e528
Hojin37 <hwlsdl37@gmail.com>
2024-04-03 23:08:01 +0900

    .

---
+++ src/main/java/com/hozn_P/project/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.hozn_P.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/controller")
+    public String main() {
+        System.out.println("컨트롤러 작동됨!");
+        return "/index";
+    }
+}
